f44cd970e1ae3e94032be7c6e6f872adb1deca8e
MenassieErmias <ermiasmenase@gmail.com>
2024-11-13 10:23:35 +0300

    /users router added in routes folder

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-  console.log('TEST');
-  response.send('Hello from homepage!')
-})
+app.get('/', (request, response) => response.send('Hello from homepage!'));
+
+app.use('/users', usersRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
