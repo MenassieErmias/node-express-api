dbea6850bc77edde8a90c669b53519692ba33eb1
MenassieErmias <ermiasmenase@gmail.com>
2024-11-13 10:14:35 +0300

    nodemon as a dev dependancy, default route testing: index.js

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get('/', (request, response) => {
+  console.log('TEST');
+  response.send('Hello from homepage!')
+})
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
