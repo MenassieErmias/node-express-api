a851007e7093d290055c57af39302315561404dd
MenassieErmias <ermiasmenase@gmail.com>
2024-11-13 11:19:27 +0300

    modified:   routes/users.js 	new file:   users.json

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    "firstName": "John",
+    "lastName": "Doe",
+    "age": 21
+  }
+];
+
 router.get('/', (req, res) => {
   res.send('hello')
 })
