8697ac6ed8b4e71d824150eb099344472a990b1a
MenassieErmias <ermiasmenase@gmail.com>
2024-11-15 14:45:51 +0300

    get one user: user.js

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,20 @@ const router = express.Router();
 
 const users = [];
 
+//get all users
 router.get('/', (req, res) => {
   res.send(users)
 });
 
+// add user
 router.post('/', (req, res) => {
-  users.push({ ...req.body, id: uuid() })
+  users.push({ ...req.body, id: uuid() });
+  res.send(users);
+})
+
+// get one user
+router.get('/:id', (req, res) => {
+  res.send(users.find(user => user.id === req.params.id));
 })
 
 
